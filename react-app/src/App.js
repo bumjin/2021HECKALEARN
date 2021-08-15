19d485905148946e3aaa8eec678561fef0375b3c
bumjin <bumjin@gmail.com>
2021-08-15 17:32:48 +0900

    UPDATE: remove time

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,6 @@ import React from "react"
 import axios from "axios"
 
 export default class App extends React.Component {
-  async getData() {
-    const res = await axios.get("http://localhost:7071/api/datetimes");
-    return res.data.message
-  }
-
   async getCatURL() {
     const res = await axios.get("https://api.thecatapi.com/v1/images/search?ref=morioh.com");
     console.log(res.data)
@@ -16,14 +11,14 @@ export default class App extends React.Component {
   
   constructor(...args) {
     super(...args); //for use 'this'
-    this.state = {data: null, catURL: null};
+    this.state = {catURL: null};
   }
 
   componentDidMount() {
     if (!this.state.data) {
       (async () => {
         try {
-          this.setState( {data: await this.getData(), catURL: await this.getCatURL() });
+          this.setState( {catURL: await this.getCatURL() });
 
         } catch(e) {
 
@@ -48,7 +43,6 @@ render() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Time : {this.state.data}
         </a>
       </header>
     </div>
